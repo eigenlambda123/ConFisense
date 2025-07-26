5d5b113a66c71a0429a3fdff0d5f1e1c5334b8df
eigenlambda123 <rmvilla987@gmail.com>
2025-07-26 14:15:10 +0800

    implemented logging in simulate_emergency_route

--- app/api/routes/simulate_emergency.py
+++ app/api/routes/simulate_emergency.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 from app.schemas.simulation_inputs import EmergencyFundInput
 from app.services.simulation_logic import simulate_emergency_fund
 
+# logging imports
+from app.models.log import SimulationLog
+from app.db.session import get_session
+
 router = APIRouter()
 
 @router.post("/simulate/emergency-fund")
@@ -22,10 +26,22 @@ def simulate_emergency_route(data: EmergencyFundInput):
     # values: [current_savings, target]
     # summary is the result of how long it will take to reach the target
     # math_explanation is the "show my math"
-    return {
+    response = {
         "labels": list(range(1, len(result["data"]) + 1)),
         "values": result["data"],
         "value_descriptions": ["Current Savings", "Target Amount"],
         "summary": result["summary"],
         "math_explanation": result["math_explanation"]
     }
+
+    # Log the simulation inputs and outputs to Database
+    with get_session() as session:
+        log = SimulationLog(
+            scenario="debt_management",
+            input_data=data.model_dump(),
+            output_data=result,
+        )
+        session.add(log)
+        session.commit()
+
+    return response
