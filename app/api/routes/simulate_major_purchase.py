2697380e309198c46b8f3f7dff17e0aae7b51d38
eigenlambda123 <rmvilla987@gmail.com>
2025-07-26 14:16:40 +0800

    implemented logging in simulate_major_purchase_route

--- app/api/routes/simulate_major_purchase.py
+++ app/api/routes/simulate_major_purchase.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 from app.schemas.simulation_inputs import MajorPurchaseInput
 from app.services.simulation_logic import simulate_major_purchase
 
+# logging imports
+from app.models.log import SimulationLog
+from app.db.session import get_session
+
 router = APIRouter()
 
 @router.post("/simulate/major-purchase")
@@ -37,11 +41,23 @@ def simulate_major_purchase_route(data: MajorPurchaseInput):
         loan_term=data.loan_term
     )
 
-    return {
+    response = {
         "labels": list(range(1, len(result["data"]) + 1)),
         "values": result["data"],
         "summary": result["summary"],
         "math_explanation": result["math_explanation"]
     }
+
+    # Log the simulation inputs and outputs to Database
+    with get_session() as session:
+        log = SimulationLog(
+            scenario="debt_management",
+            input_data=data.model_dump(),
+            output_data=result,
+        )
+        session.add(log)
+        session.commit()
+
+    return response
 
     
