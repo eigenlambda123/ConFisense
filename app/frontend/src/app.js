49885a59cc06955ef7b443cb4d550618e23b60bd
neophiles <mnv.tagle@gmail.com>
2025-08-22 19:03:06 +0800

    add category attribute in fields, add category labels, add math btn msg box

--- app/frontend/src/app.js
+++ app/frontend/src/app.js
@@ -6,21 +6,15 @@ import {
     clearChartTitle
 } from "./charts.js";
 
-import { 
-    saveEmergencyFundScenarioToDB,
-    getEmergencyFundAISuggestion,
-    getEmergencyFundAIExplaination,
-    getEmergencyFundAISummary 
-} from './emergency-fund.js';
-
 // Global states
 let currentScenario = ''; // ID of current selected scenario
 let currentScenarioConfig = {}; // Config object of active scenario
 let currentScenarioEndpoint = ''; // API endpoint for simulation
 let fields = []; // DOM input elements (for current scenario)
 let fieldValues = {}; // Store current input values
+let formulas = [];
 
-const field = (id, label, min, step, def, type='number') => ({id, label, min, step, default: def, type});
+const field = (id, label, category, min, step, def, type='number') => ({id, label, category, min, step, default: def, type});
 
 const scenariosConfig = {
     budget_optimization: {
@@ -28,37 +22,42 @@ const scenariosConfig = {
         endpoint: "/simulate/budget-optimization",
         fields: [
             // General Parameters
-            field("projection_months", "Projection Months", 12, 1, 12),
+            field("projection_months", "Projection Months", "General", 12, 1, 12),
 
             // Income Fields
-            field("monthly_net_income", "Monthly Net Income (₱)", 0, 1000, 50000),
-            field("other_monthly_income", "Other Monthly Income (₱)", 0, 500, 0),
+            field("monthly_gross_income", "Monthly Gross Income (₱)", "Income", 0, 1000, 50000),
+            field("other_monthly_income", "Other Monthly Income (₱)", "Income", 0, 500, 0),
 
             // Fixed Needs Expenses
-            field("rent_mortgage", "Rent / Mortgage (₱)", 0, 1000, 15000),
-            field("utilities", "Utilities (₱)", 0, 500, 4000),
-            field("loan_payments", "Loan Payments (₱)", 0, 100, 0),
-            field("insurance_premiums", "Insurance Premiums (₱)", 0, 100, 0),
-            field("tuition_fees", "Tuition & Fees (₱)", 0, 1000, 0),
-            field("groceries", "Groceries (₱)", 0, 500, 8000),
-            field("transportation", "Transportation (₱)", 0, 500, 3000),
+            field("rent", "Rent (₱)", "Fixed Needs", 0, 1000, 15000),
+            field("utilities", "Utilities (₱)", "Fixed Needs", 0, 500, 4000),
+            field("loan_payments", "Loan Payments (₱)", "Fixed Needs", 0, 100, 0),
+            field("insurance_premiums", "Insurance Premiums (₱)", "Fixed Needs", 0, 100, 0),
+            field("tuition_fees", "Tuition & Fees (₱)", "Fixed Needs", 0, 1000, 0),
+            field("groceries", "Food & Groceries (₱)", "Fixed Needs", 0, 500, 8000),
+            field("transportation", "Transportation (₱)", "Fixed Needs", 0, 500, 3000),
             
             // Variable Needs Expenses
-            field("household_supplies", "Household Supplies (₱)", 0, 100, 500),
-            field("medical_health", "Medical & Health (₱)", 0, 100, 0),
-            field("misc_needs", "Miscellaneous Needs (₱)", 0, 100, 0),
+            field("household_supplies", "Household Supplies (₱)", "Variable Needs", 0, 100, 500),
+            field("medical_health", "Medical & Health (₱)", "Variable Needs", 0, 100, 0),
+            field("misc_needs", "Miscellaneous Needs (₱)", "Variable Needs", 0, 100, 0),
 
             // Wants & Discretionary Expenses
-            field("dining_out", "Dining Out (₱)", 0, 100, 2500),
-            field("entertainment_hobbies", "Entertainment & Hobbies (₱)", 0, 100, 1500),
-            field("personal_care", "Personal Care (₱)", 0, 100, 0),
-            field("shopping_leisure", "Shopping & Leisure (₱)", 0, 100, 0),
-            field("travel_vacation", "Travel & Vacation (₱)", 0, 1000, 0),
-            field("misc_wants", "Miscellaneous Wants (₱)", 0, 100, 0),
+            field("dining_out", "Dining Out (₱)", "Wants", 0, 100, 2500),
+            field("entertainment_hobbies", "Entertainment & Hobbies (₱)", "Wants", 0, 100, 1500),
+            field("personal_care", "Personal Care (₱)", "Wants", 0, 100, 0),
+            field("shopping_leisure", "Shopping & Leisure (₱)", "Wants", 0, 100, 0),
+            field("travel_vacation", "Travel & Vacation (₱)", "Wants", 0, 1000, 0),
+            field("misc_wants", "Other Wants (₱)", "Wants", 0, 100, 0),
 
             // Savings Goals Fields
-            field("target_monthly_savings", "Target Monthly Savings (₱)", 0, 100, 5000),
-            field("emergency_fund_target", "Emergency Fund Target (₱)", 0, 1000, 120000),
+            field("target_monthly_savings", "Target Monthly Savings (₱)", "Goals", 0, 100, 5000),
+            field("emergency_fund_target", "Emergency Fund Target (₱)", "Goals", 0, 1000, 120000),
+
+            // What-if Factors
+            field('income_growth_rate', 'Income Growth Rate (%)', "What-If Factors", 0, 100, 0),
+            field('wants_reduction_rate', 'Wants Reduction Rate (%)', "What-If Factors", 0, 100, 0),
+            field('savings_increase_rate', 'Savings Increase Rate (%)', "What-If Factors", 0, 100, 0)
         ],
     },
     budgeting: {
@@ -103,8 +102,9 @@ const messageBox = document.getElementById('msg-box');
 const messageText = document.getElementById('msg-text');
 
 // Helper function to show custom message box
-function showMessage(message) {
-    messageText.textContent = message;
+function showMessage(message, className) {
+    messageBox.classList.add(className);
+    messageText.innerHTML = message;
     messageBox.style.display = 'block';
 }
 
@@ -159,6 +159,7 @@ function showDashboard(buttonElement) {
     // Reset state for fields and values
     fields = [];
     fieldValues = {};
+    formulas = [];
 
     // Grab reusable field template from DOM (for each input field)
     const fieldTemplate = document.getElementById('field-template');
@@ -167,8 +168,29 @@ function showDashboard(buttonElement) {
         return;
     }
 
+    let currentCategory = null;
+    let firstCategory = true;
+
     // Dynamically generate input fields based on scenario config
     currentScenarioConfig.fields.forEach(field => {
+
+        if (field.category && field.category != currentCategory) {
+            // Create new heading element for category
+            const categoryHeading = document.createElement('h3');
+            categoryHeading.textContent = field.category;
+            categoryHeading.classList.add('category-label');
+            
+            if (firstCategory) { firstCategory = false }
+            else {
+                const lineBreak = document.createElement('hr');
+                lineBreak.classList.add('category-line');
+                fieldContainer.appendChild(lineBreak);
+            }
+
+            fieldContainer.appendChild(categoryHeading);
+            currentCategory = field.category; // Update the current category
+        }
+
         // Clone the template's content
         const fieldClone = fieldTemplate.content.cloneNode(true);
         const wrapper = fieldClone.querySelector('div');
@@ -181,10 +203,6 @@ function showDashboard(buttonElement) {
         input.type = field.type;
         input.value = field.default;
         input.id = field.id;
-
-        if (field.type !== 'color') {
-            input.classList.add('non-color-input');
-        }
 
         if (field.type === 'number') {
             input.min = field.min;
@@ -209,7 +227,7 @@ function showDashboard(buttonElement) {
     openDashboard();
     clearChartTitle();
     //clearAIResponses();
-    renderChart(currentScenario); // Draw scenario-specific chart after form is set up
+    renderChart(currentScenario);
 }
 
 function showHome() {
@@ -222,54 +240,56 @@ function showHome() {
     destroyChart();
 }
 
-function transformFrontendData(data) {
-    const apiBody = {
-        scenario_type: data.scenario_type,
-        user_type: data.user_type,
-        projection_months: data.projection_months,
+// Only for budget optimization for now
+const buildRequestBody = (flatData) => {
+    const body = {
+        scenario_type: flatData.scenario_type,
+        user_type: flatData.user_type,
+        projection_months: flatData.projection_months,
         income: {
-            monthly_net_income: data.monthly_net_income,
-            other_monthly_income: data.other_monthly_income,
+            monthly_gross_income: flatData.monthly_gross_income,
+            other_monthly_income: flatData.other_monthly_income,
             income_frequency: "monthly"
         },
         expenses: {
             fixed_needs: {
-                rent_mortgage: data.rent_mortgage,
-                utilities: data.utilities,
-                loan_payments: data.loan_payments,
-                insurance_premiums: data.insurance_premiums,
-                tuition_fees: data.tuition_fees,
-                groceries: data.groceries,
-                transportation: data.transportation
+                rent: flatData.rent,
+                utilities: flatData.utilities,
+                loan_payments: flatData.loan_payments,
+                insurance_premiums: flatData.insurance_premiums,
+                tuition_fees: flatData.tuition_fees,
+                groceries: flatData.groceries,
+                transportation: flatData.transportation
             },
             variable_needs: {
-                household_supplies: data.household_supplies,
-                medical_health: data.medical_health,
-                misc_needs: data.misc_needs
+                household_supplies: flatData.household_supplies,
+                medical_health: flatData.medical_health,
+                misc_needs: flatData.misc_needs
             },
             wants_discretionary: {
-                dining_out: data.dining_out,
-                entertainment_hobbies: data.entertainment_hobbies,
-                personal_care: data.personal_care,
-                shopping_leisure: data.shopping_leisure,
-                travel_vacation: data.travel_vacation,
-                misc_wants: data.misc_wants
+                dining_out: flatData.dining_out,
+                entertainment_hobbies: flatData.entertainment_hobbies,
+                personal_care: flatData.personal_care,
+                shopping_leisure: flatData.shopping_leisure,
+                travel_vacation: flatData.travel_vacation,
+                misc_wants: flatData.misc_wants
             }
         },
         savings_goals: {
-            target_monthly_savings: data.target_monthly_savings,
-            emergency_fund_target: data.emergency_fund_target
+            target_monthly_savings: flatData.target_monthly_savings,
+            emergency_fund_target: flatData.emergency_fund_target
+        },
+        what_if_factors: {
+            income_growth_rate: flatData.income_growth_rate / 100,
+            wants_reduction_rate: flatData.wants_reduction_rate / 100,
+            savings_increase_rate: flatData.savings_increase_rate / 100
         }
     };
-
-    return apiBody;
-}
+    return body;
+};
 
 async function runSimulation(endpoint, params) {
-    // Prepare request payload (parameters for simulation scenario)
-    console.log(params);
-    const requestBody = transformFrontendData(params);
-
+    const requestBody = buildRequestBody(params); // Prepare request payload
     console.log(requestBody); // FOR DEBUGGING
 
     try {
@@ -291,31 +311,22 @@ async function runSimulation(endpoint, params) {
         const result = await response.json();
         const chartData = result.data.chart_data;
         const inputsReceived = result.data.inputs_received;
-        const formulas = result.data.show_my_math;
+        const insight = result.data.insight;
+        const keyMetrics = result.data.key_metrics;
+        formulas = result.data.show_my_math;
         
         // FOR DEBUGGING
         console.log('Result: ', result);
         console.log('Chart Data: ', chartData);
+        console.log('Insight: ', insight);
+        console.log('Key Metrics: ', keyMetrics);
         console.log('Inputs Received: ', inputsReceived);
         console.log('Formulas: ', formulas); 
 
         addDatasets(chartData);
-        fetchAndRenderAISummary();
 
     } catch (err) {
         console.error('Fetch error:', err);
-    }
-}
-
-async function fetchAndRenderAISummary() {
-    try {
-        console.log('Fetching AI Summary...');
-        const summaryResult = await getEmergencyFundAISummary();
-        const ai_summary = summaryResult.summary || "No summary available.";
-        updateChartTitle(ai_summary);
-
-    } catch (error) {
-        console.error('Error fetching AI summary:', error);
     }
 }
 
@@ -419,7 +430,7 @@ document.getElementById('clear-btn').addEventListener('click', function() {
 document.getElementById('simulate-btn').addEventListener('click', async (event) => {
     // Pass chosen scenario and collected input values to backend
     if (areInputFieldsEmpty()) {
-        showMessage("Please fill in all input fields before running the simulation.");
+        showMessage("Please fill in all input fields before running the simulation.", 'empty-field-msg-box');
         console.warn("Simulation prevented: Some input fields are empty.");
         return; // Stop the function if fields are empty
     }
@@ -428,6 +439,14 @@ document.getElementById('simulate-btn').addEventListener('click', async (event) 
 
 document.getElementById('explain-btn').addEventListener('click', async (event) => {
     await fetchAndRenderAIExplanation();
+});
+
+document.getElementById('math-btn').addEventListener('click', async (event) => {
+    if (formulas.length === 0) {
+        showMessage('Run simulation first to see formulas.' ,'math-msg-box');
+        return;
+    }
+    showMessage(`Formulas Used<br><br>${formulas.join('<br><br>')}` ,'math-msg-box');
 });
 
 document.getElementById('suggest-btn').addEventListener('click', async (event) => {
